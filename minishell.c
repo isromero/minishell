414153ebfa59e3a60c0d95b887cb0e8f388ecc6f
isromero <ismaromeroortega@gmail.com>
2023-05-18 21:50:53 +0200

    prompt visual terminado con colores

--- minishell.c
+++ minishell.c
@@ -24,23 +24,42 @@ void	parse_args(t_cmd *cmd)
 	cmd->n_tokens = i;
 }
 
-char *get_prompt(t_cmd *cmd)
+char *get_prompt(t_cmd *custom_prompt)
 {
-	char* args[] = { "bash", "-c", "echo -n \"$(whoami)@minishell:\"; pwd", NULL };
-	return(execve("bash", args));
+    char *username = getenv("USER"); // Obtener el nombre de usuario desde la variable de entorno USER
+    char cwd[1024]; 
+    getcwd(cwd, sizeof(cwd)); // Obtener la ubicación actual de la terminal
+    
+    size_t prompt_length = ft_strlen(username) + ft_strlen(cwd) + 13 + ft_strlen(COLOR_GREEN) \
+	+ ft_strlen(COLOR_MAGENTA) + ft_strlen(COLOR_YELLOW) + ft_strlen(COLOR_CYAN) + ft_strlen(COLOR_WHITE); // Longitud total del prompt personalizado
+    custom_prompt->prompt = (char *)malloc(prompt_length * sizeof(char));
+
+	strcpy(custom_prompt->prompt, COLOR_GREEN);
+    strcat(custom_prompt->prompt, username);
+    strcat(custom_prompt->prompt, COLOR_MAGENTA);
+    strcat(custom_prompt->prompt, "@minishell");
+	strcat(custom_prompt->prompt, COLOR_YELLOW);
+	strcat(custom_prompt->prompt, ":");
+    strcat(custom_prompt->prompt, COLOR_CYAN);
+    strcat(custom_prompt->prompt, cwd);
+    strcat(custom_prompt->prompt, COLOR_WHITE);
+    strcat(custom_prompt->prompt, "$ ");
+    
+    return (custom_prompt->prompt);
 }
 
 int	main(int argc, char **argv)
 {
 	t_cmd	cmd;
-	char	*prompt;
 	
-	parse_args(&cmd);
-	prompt = get_prompt(&cmd);
+	
 	while(1)
 	{
-		cmd.line = readline(prompt);
+		cmd.prompt = get_prompt(&cmd);
+		cmd.line = readline(cmd.prompt);
+		parse_args(&cmd);
 		free(cmd.line);
+		free(cmd.prompt);
 	}
     return (0);
 }
--- minishell.h
+++ minishell.h
@@ -21,6 +21,31 @@
 #include <readline/history.h>
 #include "libft/libft.h"
 
+
+#define STYLE_BOLD "\033[1m"
+#define STYLE_UNDERLINE "\033[4m"
+#define STYLE_BLINK "\033[5m"
+
+// Colores de texto
+#define COLOR_BLACK "\033[30m"
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_YELLOW "\033[33m"
+#define COLOR_BLUE "\033[34m"
+#define COLOR_MAGENTA "\033[35m"
+#define COLOR_CYAN "\033[36m"
+#define COLOR_WHITE "\033[37m"
+
+// Colores de fondo
+#define BACKGROUND_BLACK "\033[40m"
+#define BACKGROUND_RED "\033[41m"
+#define BACKGROUND_GREEN "\033[42m"
+#define BACKGROUND_YELLOW "\033[43m"
+#define BACKGROUND_BLUE "\033[44m"
+#define BACKGROUND_MAGENTA "\033[45m"
+#define BACKGROUND_CYAN "\033[46m"
+#define BACKGROUND_WHITE "\033[47m"
+
 typedef struct cmd
 {
 	char *line;

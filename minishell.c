7afa7571dd2458e7c2eb0c366f7254bd9d2be6f9
Adrian Gutierrez segovia <adgutier@c1r5s2.42madrid.com>
2023-09-12 15:44:09 +0200

    quitada linea rl_replaceline para que compile en 42

--- minishell.c
+++ minishell.c
@@ -58,7 +58,7 @@ int	main(int argc, char **argv, char **env)
 	unlink("/tmp/heredoc");
 	unlink("/tmp/heredoc_expanded");
 	init_minishell(&cmd, sig);
-	rl_clear_history();
+	clear_history();
 	free_matrix(cmd.env);
 	return (g_status);
 }
--- signals.c
+++ signals.c
@@ -31,7 +31,6 @@ void	handle_ctrlc(int sig)
 	(void)sig;
 	ft_putchar_fd('\n', STDOUT_FILENO);
 	rl_on_new_line();
-	rl_replace_line("", 0);
 	rl_redisplay();
 	g_status = 130;
 }
